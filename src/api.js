825c3c0628107e587d865d640f15fdd32835f7b2
dinesh <160705316+Dinesh120702@users.noreply.github.com>
2024-02-26 20:05:52 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9ba6d5e465msh5429e131e4a6710p1753d9jsn65e8e3765886",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap./data/2.5";
+export const WEATHER_API_KEY = "aab4ae0b4f25cccf9cb8270f4e40672b"; 
